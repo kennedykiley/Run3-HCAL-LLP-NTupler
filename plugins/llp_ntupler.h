8c0330a6a1d40371379c3f742f1d9951dd2cdda1
Cristian <cristian.morgoth@gmail.com>
2019-09-12 06:40:54 +0200

    add Jingyu Displaced Trigger for 2017 to the trigger list

--- plugins/llp_ntupler.h
+++ plugins/llp_ntupler.h
@@ -243,7 +243,7 @@ protected:
   string eleHLTFilterNamesFile_;
   string muonHLTFilterNamesFile_;
   string photonHLTFilterNamesFile_;
-  static const int NTriggersMAX = 601;
+  static const int NTriggersMAX = 602;
   string triggerPathNames[NTriggersMAX];
   static const int MAX_ElectronHLTFilters = 100;
   string eleHLTFilterNames[MAX_ElectronHLTFilters];
