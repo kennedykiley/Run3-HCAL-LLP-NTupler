d0488d7f9e88661161b285ca799d8d2ad0765c5e
sixie <si.xie@cern.ch>
2019-10-10 02:42:40 +0200

    add displacedJetTiming ntupler

---
+++ plugins/displacedJetTiming_aux.cc
@@ -0,0 +1,45 @@
+#include "displacedJetTiming_ntupler.h"
+
+const reco::Candidate* displacedJetTiming_ntupler::findFirstMotherWithDifferentID(const reco::Candidate *particle){
+
+  if( particle == 0 ){
+    printf("ERROR! null candidate pointer, this should never happen\n");
+    return 0;
+  }
+
+  // Is this the first parent with a different ID? If yes, return, otherwise
+  // go deeper into recursion
+  if (particle->numberOfMothers() > 0 && particle->pdgId() != 0) {
+    if (particle->pdgId() == particle->mother(0)->pdgId()
+	&& particle->mother(0)->status() != 11  // prevent infinite loop for sherpa documentation gluons
+	) {
+      return findFirstMotherWithDifferentID(particle->mother(0));
+    } else {
+      return particle->mother(0);
+    }
+  }
+
+  return 0;
+};
+
+const reco::Candidate* displacedJetTiming_ntupler::findOriginalMotherWithSameID(const reco::Candidate *particle){
+
+  if( particle == 0 ){
+    printf("ERROR! null candidate pointer, this should never happen\n");
+    return 0;
+  }
+
+  // Is there another parent with the same ID? If yes, go deeper into recursion
+  if (particle->numberOfMothers() > 0 && particle->pdgId() != 0) {
+    if (particle->mother(0)->numberOfMothers() == 0 ||
+	particle->mother(0)->status() == 11 ||  // prevent infinite loop for sherpa documentation gluons
+	(particle->mother(0)->numberOfMothers() > 0 && particle->mother(0)->mother(0)->pdgId() != particle->mother(0)->pdgId())
+	) {
+      return particle->mother(0);
+    } else {
+      return findOriginalMotherWithSameID(particle->mother(0));
+    }
+  }
+
+  return 0;
+}
