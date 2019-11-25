5b30e14bfccb455139c5aaa24228879a334564ae
christinaw97 <christina.wang@caltech.edu>
2019-11-25 12:09:31 -0800

    added scripts

---
+++ python/displacedJetMuon_step2_ntupler_MC_Summer16_cfg_condor.py
@@ -0,0 +1,216 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step2 --filein file:EXO-RunIISummer16DR80-00005_step1.root --fileout file:EXO-RunIISummer16DR80-00005.root --mc --eventcontent RAWAODSIM,DQM --runUnscheduled --datatier RAWAODSIM,DQMIO --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step RAW2DIGI,L1Reco,RECO,EI,DQM:DQMOfflinePOGMC --era Run2_2016 --python_filename EXO-RunIISummer16DR80-00005_2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('RECO',eras.Run2_2016)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('CommonTools.ParticleFlow.EITopPAG_cff')
+process.load('DQMOffline.Configuration.DQMOfflineMC_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    #inputfilename   
+    ),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    allowUnscheduled = cms.untracked.bool(True)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step2 nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+#TFileService for output
+process.TFileService = cms.Service("TFileService",
+        fileName = cms.string('outputfilename'),
+    closeFileFast = cms.untracked.bool(True)
+)
+
+process.ntuples = cms.EDAnalyzer('displacedJetMuon_ntupler',
+    isData = cms.bool(False),
+    isAOD = cms.bool(False),
+    useGen = cms.bool(True),
+    isFastsim = cms.bool(False),
+    enableTriggerInfo = cms.bool(False),
+    enableEcalRechits = cms.bool(False),
+    enableCaloJet = cms.bool(True),
+    enableGenLLPInfo = cms.bool(True),
+    readGenVertexTime = cms.bool(False),#need to be false for displaced samples
+    genParticles_t0 = cms.InputTag("genParticles", "t0", ""),
+    triggerPathNamesFile = cms.string("cms_lpc_llp/llp_ntupler/data/trigger_names_llp_v1.dat"),
+    eleHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorElectronHLTFilterNames.dat"),
+    muonHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorMuonHLTFilterNames.dat"),
+    photonHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorPhotonHLTFilterNames.dat"),
+
+    #vertices = cms.InputTag("offlinePrimaryVerticesWithBS"),  # for non-timing case
+    vertices = cms.InputTag("offlinePrimaryVertices", "", "RECO"),
+    muons = cms.InputTag("muons"),
+    electrons = cms.InputTag("gedGsfElectrons"),
+    taus = cms.InputTag("hpsPFTauProducer"),
+    photons = cms.InputTag("gedPhotons"),
+    jetsCalo = cms.InputTag("ak4CaloJets","","RECO"),
+    jetsPF = cms.InputTag("ak4PFJets"),
+    jets = cms.InputTag("ak4PFJetsCHS"),
+    jetsPuppi = cms.InputTag("ak4PFJets"),
+    jetsAK8 = cms.InputTag("ak8PFJetsCHS"),
+    mets = cms.InputTag("pfMet"),
+    #metsNoHF = cms.InputTag("pfMet30"),
+    metsPuppi = cms.InputTag("pfMet"),
+    pfCands = cms.InputTag("particleFlow","","RECO"),
+genParticles = cms.InputTag("genParticles"),
+    MuonCSCSimHits = cms.InputTag("g4SimHits", "MuonCSCHits","SIM"),
+    MuonCSCComparatorDigi = cms.InputTag("simMuonCSCDigis", "MuonCSCComparatorDigi", "HLT"),
+    MuonCSCStripDigi = cms.InputTag("simMuonCSCDigis", "MuonCSCStripDigi", "HLT"),
+    MuonCSCWireDigi = cms.InputTag("simMuonCSCDigis", "MuonCSCWireDigi", "HLT"),
+    MuonCSCWireDigiSimLinks = cms.InputTag( "simMuonCSCDigis", "MuonCSCWireDigiSimLinks", "HLT"),
+    MuonCSCStripDigiSimLinks = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigiSimLinks", "HLT"),
+    #packedGenParticles = cms.InputTag("packedGenParticles"),
+    #prunedGenParticles = cms.InputTag("prunedGenParticles"),
+    genMetsCalo = cms.InputTag("genMetCalo"),
+    genMetsTrue = cms.InputTag("genMetTrue"),
+    genJets = cms.InputTag("ak4GenJets"),
+
+    triggerBits = cms.InputTag("TriggerResults","","HLT"),
+    #triggerBits = cms.InputTag("TriggerResults","","RECO"),
+    hepMC = cms.InputTag("generatorSmeared", "", "SIM"),
+
+    #triggerPrescales = cms.InputTag("patTrigger"),
+    #triggerObjects = cms.InputTag("selectedPatTrigger"),
+
+    metFilterBits = cms.InputTag("TriggerResults", "", "RECO"),
+
+    #hbheNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHENoiseFilterResult"),
+    #hbheTightNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHENoiseFilterResultRun2Tight"),
+    #hbheIsoNoiseFilter = cms.InputTag("HBHENoiseFilterResultProducer","HBHEIsoNoiseFilterResult"),
+
+    #BadChargedCandidateFilter = cms.InputTag("BadChargedCandidateFilter",""),
+    #BadMuonFilter = cms.InputTag("BadPFMuonFilter",""),
+
+    #lheInfo = cms.InputTag("externalLHEProducer", "", ""),
+    genInfo = cms.InputTag("generator", "", "SIM"),
+
+    tracks = cms.InputTag("generalTracks", "", "RECO"),
+    #trackTime = cms.InputTag("trackTimeValueMapProducer","generalTracksConfigurableFlatResolutionModel"),
+    #trackTimeReso = cms.InputTag("trackTimeValueMapProducer","generalTracksConfigurableFlatResolutionModelResolution"),
+
+    puInfo = cms.InputTag("addPileupInfo", "", "HLT"), #uncomment if no pre-mixing
+    #puInfo = cms.InputTag("mixData", "", "HLT"), #uncomment for samples with pre-mixed pileup
+    #hcalNoiseInfo = cms.InputTag("hcalnoise", "", "RECO"),
+
+    #secondaryVertices = cms.InputTag("inclusiveSecondaryVertices", "", "RECO"),
+    secondaryVertices = cms.InputTag("inclusiveCandidateSecondaryVertices","", "RECO"),
+
+    rhoAll = cms.InputTag("fixedGridRhoAll", "", "RECO"),
+    rhoFastjetAll = cms.InputTag("fixedGridRhoFastjetAll", "", "RECO"),
+    rhoFastjetAllCalo = cms.InputTag("fixedGridRhoFastjetAllCalo", "", "RECO"),
+    rhoFastjetCentralCalo = cms.InputTag("fixedGridRhoFastjetCentralCalo", "", "RECO"),
+    rhoFastjetCentralChargedPileUp = cms.InputTag("fixedGridRhoFastjetCentralChargedPileUp", "", "RECO"),
+    rhoFastjetCentralNeutral = cms.InputTag("fixedGridRhoFastjetCentralNeutral", "", "RECO"),
+
+    beamSpot = cms.InputTag("offlineBeamSpot", "", "RECO"),
+    pfClusters = cms.InputTag("particleFlowClusterECAL","","RECO"),
+    ebRecHits = cms.InputTag("reducedEcalRecHitsEB", "","RECO"),
+    #ebRecHits = cms.InputTag("EcalRecHit", "reducedEcalRecHitsEB", "RECO"),
+    eeRecHits  = cms.InputTag("reducedEcalRecHitsEE", "","RECO"),
+    esRecHits = cms.InputTag("reducedEcalRecHitsES", "","RECO"),
+    #ebeeClusters = cms.InputTag("reducedEgamma", "reducedEBEEClusters", "RECO"),
+    ebeeClusters = cms.InputTag("particleFlowEGamma", "EBEEClusters", "RECO"),
+    esClusters = cms.InputTag("particleFlowEGamma", "ESClusters", "RECO"),
+    #conversions = cms.InputTag("reducedEgamma", "reducedConversions", "RECO"),
+    conversions = cms.InputTag("allConversions", "", "RECO"),
+
+    #singleLegConversions = cms.InputTag("reducedEgamma", "reducedSingleLegConversions", "RECO"),
+    singleLegConversions = cms.InputTag("particleFlowEGamma", "", "RECO"),
+
+    gedGsfElectronCores = cms.InputTag("gedGsfElectronCores", "", "RECO"),
+    gedPhotonCores = cms.InputTag("gedPhotonCore", "", "RECO"),
+    #superClusters = cms.InputTag("reducedEgamma", "reducedSuperClusters", "RECO"),
+
+    #lostTracks = cms.InputTag("lostTracks", "", "RECO")
+)
+
+
+# Output definition
+
+process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('AODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(31457280),
+    fileName = cms.untracked.string('file:SUS-RunIISummer16DRPremix-00183.root'),
+    outputCommands = process.AODSIMEventContent.outputCommands
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v6', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+#process.recosim_step = cms.Path(process.recosim)
+process.eventinterpretaion_step = cms.Path(process.EIsequence)
+process.ntupler_step = cms.Path(process.ntuples)
+#process.endjob_step = cms.EndPath(process.endOfProcess)
+#process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+process.dqmoffline_step = cms.Path(process.DQMOfflinePOGMC)
+process.dqmofflineOnPAT_step = cms.Path(process.DQMOfflinePOGMC)
+process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
+#process.DQMoutput_step = cms.EndPath(process.DQMoutput)
+
+# Schedule definition
+#process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.eventinterpretaion_step,process.dqmoffline_step,process.dqmofflineOnPAT_step,process.RAWAODSIMoutput_step,process.DQMoutput_step)
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.eventinterpretaion_step,process.dqmoffline_step,process.dqmofflineOnPAT_step,process.ntupler_step)
+#process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.eventinterpretaion_step,process.endjob_step,process.ntupler_step)
+
+#from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+#associatePatAlgosToolsTask(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+from FWCore.ParameterSet.Utilities import cleanUnscheduled
+process=cleanUnscheduled(process)
+
