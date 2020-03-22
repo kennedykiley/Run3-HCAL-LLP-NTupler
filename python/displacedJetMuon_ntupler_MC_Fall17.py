8c181afd9c73385c5aa1ec21bd47e84908bf878d
sixie <si.xie@cern.ch>
2020-03-22 20:02:55 +0100

    add Fall17 config

--- python/displacedJetMuon_ntupler_MC_Fall17.py
+++ python/displacedJetMuon_ntupler_MC_Fall17.py
@@ -5,9 +5,9 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 #initialize the process
 process = cms.Process("displacedJetMuonNtupler")
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
 process.load("Configuration.EventContent.EventContent_cff")
 process.load("cms_lpc_llp.llp_ntupler.metFilters_cff_2017")
+
 
 #load input files
 process.source = cms.Source("PoolSource",
@@ -18,8 +18,12 @@ process.source = cms.Source("PoolSource",
         )
 )
 
+process.options = cms.untracked.PSet(
+
+)
+
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
-process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 #TFileService for output
 process.TFileService = cms.Service("TFileService",
@@ -29,19 +33,55 @@ process.TFileService = cms.Service("TFileService",
 
 #load run conditions
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.load('Configuration.Geometry.GeometryIdeal_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+#process.load('Configuration.Geometry.GeometryIdeal_cff')
+#process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 
 #------ Declare the correct global tag ------#
 
 
 process.GlobalTag.globaltag = '94X_mc2017_realistic_v17'
 
-#------ If we add any inputs beyond standard miniAOD event content, import them here ------#
-
+#------ If we add any inputs beyond standard event content, import them here ------#
 process.load('RecoMET.METFilters.BadPFMuonFilter_cfi')
 
+
+process.output = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string(''),
+        filterName = cms.untracked.string('')
+    ),
+    dropMetaData = cms.untracked.string('ALL'),
+    eventAutoFlushCompressedSize = cms.untracked.int32(-900),
+    fastCloning = cms.untracked.bool(False),
+    fileName = cms.untracked.string('miniAOD-prod_PAT.root'),
+    outputCommands = cms.untracked.vstring('keep *'),
+)
+
+
+
+  
 #------ Analyzer ------#
+
+# For AOD Track variables
+process.load("RecoTracker.TkNavigation.NavigationSchoolESProducer_cfi")
+process.MaterialPropagator = cms.ESProducer('PropagatorWithMaterialESProducer',
+    ComponentName = cms.string('PropagatorWithMaterial'),
+    Mass = cms.double(0.105),
+    MaxDPhi = cms.double(1.6),
+    PropagationDirection = cms.string('alongMomentum'),
+    SimpleMagneticField = cms.string(''),
+    ptMin = cms.double(-1.0),
+    useRungeKutta = cms.bool(False)
+)
+
+process.TransientTrackBuilderESProducer = cms.ESProducer('TransientTrackBuilderESProducer',
+    ComponentName = cms.string('TransientTrackBuilder')
+)
+
 
 #list input collections
 process.ntuples = cms.EDAnalyzer('displacedJetMuon_ntupler',
@@ -55,7 +95,7 @@ process.ntuples = cms.EDAnalyzer('displacedJetMuon_ntupler',
     enableGenLLPInfo = cms.bool(True),
     readGenVertexTime = cms.bool(False),#need to be false for displaced samples
     genParticles_t0 = cms.InputTag("genParticles", "t0", ""),
-    triggerPathNamesFile = cms.string("cms_lpc_llp/llp_ntupler/data/trigger_names_llp_v1.dat"),
+    triggerPathNamesFile = cms.string("cms_lpc_llp/llp_ntupler/data/trigger_names_llp_v3.dat"),
     eleHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorElectronHLTFilterNames.dat"),
     muonHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorMuonHLTFilterNames.dat"),
     photonHLTFilterNamesFile = cms.string("SUSYBSMAnalysis/RazorTuplizer/data/RazorPhotonHLTFilterNames.dat"),
@@ -70,7 +110,9 @@ process.ntuples = cms.EDAnalyzer('displacedJetMuon_ntupler',
     jetsPF = cms.InputTag("ak4PFJets"),
     jets = cms.InputTag("ak4PFJetsCHS"),
     jetsPuppi = cms.InputTag("ak4PFJets"),
-    jetsAK8 = cms.InputTag("ak8PFJetsCHS"),
+    #jetsAK8 = cms.InputTag("ak8PFJetsCHS"),
+    jetsAK8 = cms.InputTag("selectedPatJetsAK8PFCHS"),
+
     mets = cms.InputTag("pfMet"),
     #metsNoHF = cms.InputTag("pfMet30"),
     metsPuppi = cms.InputTag("pfMet"),
@@ -147,10 +189,37 @@ process.ntuples = cms.EDAnalyzer('displacedJetMuon_ntupler',
 
     gedGsfElectronCores = cms.InputTag("gedGsfElectronCores", "", "RECO"),
     gedPhotonCores = cms.InputTag("gedPhotonCore", "", "RECO"),
+    generalTracks = cms.InputTag("generalTracks", "", "RECO"),
     #superClusters = cms.InputTag("reducedEgamma", "reducedSuperClusters", "RECO"),
 
     #lostTracks = cms.InputTag("lostTracks", "", "RECO")
 )
 
-#run
-process.p = cms.Path( process.metFilters * process.ntuples)
+#Add jettiness for AK8 jets
+process.load('RecoJets.JetProducers.nJettinessAdder_cfi')
+process.NjettinessAK8CHS = process.Njettiness.clone()
+
+#Define Execution Paths
+process.outputPath = cms.EndPath(process.output)
+process.p = cms.Path(process.NjettinessAK8CHS * process.metFilters * process.ntuples )
+process.schedule = cms.Schedule(process.p)
+
+
+#Define Jet Tool Box Stuff
+listBtagDiscriminatorsAK4 = [ 
+                'pfJetProbabilityBJetTags',
+                'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+                'pfCombinedMVAV2BJetTags',
+                'pfCombinedCvsLJetTags',
+                'pfCombinedCvsBJetTags',
+                ]
+from JMEAnalysis.JetToolbox.jetToolbox_cff import jetToolbox
+#jetToolbox( process, 'ak8', 'ak8JetSubs', 'jetSequence', PUMethod='CHS', bTagDiscriminators=listBtagDiscriminatorsAK4, addPruning=True, addSoftDrop=True, addTrimming=True, addFiltering=True, addMassDrop=True, addNsub=True, addNsubSubjets=True, addPrunedSubjets=True, addPUJetID=True, addQJets=True, addQGTagger=True, miniAOD=False )   ### For example
+jetToolbox( process, 'ak8', 'ak8JetSubs', "out", PUMethod='CHS', bTagDiscriminators=listBtagDiscriminatorsAK4, addSoftDrop=True, addNsub=True, addNsubSubjets=True, miniAOD=False )   ### For example
+
+
+#Add PAT tasks for jet Toolbox to execution schedule
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+
