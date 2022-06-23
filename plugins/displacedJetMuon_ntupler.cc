9dcb880a293d82d345059689ce8c2312977e2e3d
sixie <si.xie@cern.ch>
2022-06-23 21:12:44 +0200

    fix some bugs to make it compatible with CMSSW 10_6

--- plugins/displacedJetMuon_ntupler.cc
+++ plugins/displacedJetMuon_ntupler.cc
@@ -5382,7 +5382,7 @@ bool displacedJetMuon_ntupler::fillElectrons(const edm::Event& iEvent)
     ele_PassConvVeto[nElectrons] = false;
     if( beamSpot.isValid() && conversions.isValid() )
     {
-      ele_PassConvVeto[nElectrons] = !ConversionTools::hasMatchedConversion(ele,conversions,beamSpot->position());
+      ele_PassConvVeto[nElectrons] = !ConversionTools::hasMatchedConversion(ele,(*conversions),beamSpot->position());
     } else {
       cout << "\n\nERROR!!! conversions not found!!!\n";
     }
@@ -5515,11 +5515,11 @@ bool displacedJetMuon_ntupler::fillPhotons(const edm::Event& iEvent, const edm::
     // Fill default miniAOD isolation quantities
     //------------------------------------------
     pho_pfIsoChargedHadronIso[nPhotons] = pho.chargedHadronIso();
-    pho_pfIsoChargedHadronIsoWrongVtx[nPhotons] = pho.chargedHadronIsoWrongVtx();
+    //pho_pfIsoChargedHadronIsoWrongVtx[nPhotons] = pho.chargedHadronIsoWrongVtx();
     pho_pfIsoNeutralHadronIso[nPhotons] = pho.neutralHadronIso();
     pho_pfIsoPhotonIso[nPhotons] = pho.photonIso();
-    pho_pfIsoModFrixione[nPhotons] = pho.getPflowIsolationVariables().modFrixione;
-    pho_pfIsoSumPUPt[nPhotons] = pho.sumPUPt();
+    //pho_pfIsoModFrixione[nPhotons] = pho.getPflowIsolationVariables().modFrixione;
+    //pho_pfIsoSumPUPt[nPhotons] = pho.sumPUPt();
     //-----------------------
     // super cluster position and time
     //-----------------------
